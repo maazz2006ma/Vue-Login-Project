4c86a39addba32a2b935f29425b563be09efce82
maazz2006ma <maazz2006.ma@gmail.com>
2024-02-21 23:53:12 +0500

    router added

---
+++ src/components/Views/LoginCard.vue
@@ -0,0 +1,9 @@
+<template>
+<form @submit.prevent="">
+  <label for="">email</label>
+  <input type="text">
+  <label for="">password</label>
+  <input type="text">
+  <button type="submit"></button>
+</form>
+</template>
