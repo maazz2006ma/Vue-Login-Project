11df08daba795cb416a17e9f863689b3eb866824
maazz2006ma <maazz2006.ma@gmail.com>
2024-02-24 00:29:09 +0500

    configured Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,12 @@
 <template>
-  <RouterLink to="/login" @click.prevent="enableRoute"> Login</RouterLink>
-  <RouterLink to="/signup" @click.prevent="enableRoute"> SignUp</RouterLink>
-
+  <v-app>
+    <v-btn color="primary">Primary</v-btn>
+  </v-app>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue';
-const isLogin =  ref(false)
-
-function enableRoute(){
-  isLogin.value = !isLogin.value
-}
-
+<script lang="ts">
+import { defineComponent } from 'vue';
+export default defineComponent({
+  name: 'App',
+});
 </script>
-
